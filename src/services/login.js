a6954350bed821c5939852adb67ec4d6cbd1fc4e
Liaoxiuting <971101lxt>
2019-09-20 21:44:44 +0800

    lxt 自适应 排版还有 移动版 没完成

--- src/services/login.js
+++ src/services/login.js
@@ -10,3 +10,23 @@ export let UserRegister =(params)=>{
     let url ='/api/emstu/teacher/register';
     return request.post(url,params)
 }
+// 添加成绩   /emstu/student/addrecord
+export let Useraddrecord =(params)=>{
+    let url ='/api/emstu/student/addrecord';
+    return request.put(url,params)
+}
+// 创建班级 /emstu/class/create
+export let Usercreate =(params)=>{
+    let url ='/api/emstu/class/create';
+    return request.put(url,params)
+}
+// 成绩列表 /emstu/student/recordlists
+export let Userrecordlists =(params)=>{
+    let url ='/emstu/student/recordlists';
+    return request.get(url,params)
+}
+// 重点名单 /emstu/student/lists
+export let Userlists =(params)=>{
+    let url ='/api/emstu/student/lists';
+    return request.get(url,params)
+}
